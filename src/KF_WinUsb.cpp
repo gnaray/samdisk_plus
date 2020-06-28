06bd72082c720d49374e35dbbeb74d7ac34f55de
Simon Owen <simon@simonowen.com>
2020-06-28 14:41:58 +0100

    Updated to require C++17
    Replaced use of features deprecated in C++17.
    
    Dropped Travis CI Xcode 9.4.1 build, added Xcode 12 build.

--- src/KF_WinUsb.cpp
+++ src/KF_WinUsb.cpp
@@ -7,7 +7,6 @@
 #include "Util.h"
 #include "win32_error.h"
 
-#include <codecvt>
 #include <setupapi.h>
 
 #ifndef INITGUID
@@ -138,10 +137,13 @@ std::string KF_WinUsb::GetProductName()
 
         if (ret && strdesc->bLength > 2)
         {
-            auto wchars = (strdesc->bLength - sizeof(USB_STRING_DESCRIPTOR)) / sizeof(WCHAR) + 1;
-            auto product = std::wstring(strdesc->bString, wchars);
-            std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
-            return conv.to_bytes(product);
+            int wchars = (strdesc->bLength - sizeof(USB_STRING_DESCRIPTOR)) / sizeof(WCHAR) + 1;
+            auto size_bytes = WideCharToMultiByte(
+                CP_ACP, 0, strdesc->bString, wchars, nullptr, 0, nullptr, nullptr);
+            std::string product(size_bytes, 0);
+            WideCharToMultiByte(
+                CP_ACP, 0, strdesc->bString, wchars, product.data(), size_bytes, nullptr, nullptr);
+            return product;
         }
     }
 
