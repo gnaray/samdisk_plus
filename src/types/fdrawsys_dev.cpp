f6f355d7bd3c696ea7a23daf5e322305c415ce88
Gábor Náray <drszamo@gmail.com>
2023-07-09 16:10:39 +0200

    Removed opt_{rpm,rpm_time_tolerance_permille} from fdrawsys_dev.cpp and modified handlong of wrong rpm.
    Earlier those were added to customise the rpm checking of tracks.
    However those are not necessary by the new handling of tracktime.
    In older (<1.0.1.12) driver the tracktime is corrupted rarely. Detecting
    it and retrying the scanning is the new solution which also fine tunes
    the encoding and data rate detecting.

--- src/types/fdrawsys_dev.cpp
+++ src/types/fdrawsys_dev.cpp
@@ -26,10 +26,10 @@ static auto& opt_gaps = getOpt<int>("gaps");
 static auto& opt_newdrive = getOpt<int>("newdrive");
 static auto& opt_normal_disk = getOpt<bool>("normal_disk");
 static auto& opt_retries = getOpt<int>("retries");
-static auto& opt_rpm = getOpt<int>("rpm");
-static auto& opt_rpm_time_tolerance_permille = getOpt<int>("rpm_time_tolerance_permille");
 static auto& opt_sectors = getOpt<long>("sectors");
 static auto& opt_steprate = getOpt<int>("steprate");
+
+constexpr uint32_t DriverVersion1_0_1_12 = 0x0100010c;
 
 class FdrawSysDevDisk final : public DemandDisk
 {
@@ -176,6 +176,7 @@ TrackData FdrawSysDevDisk::load(const CylHead& cylhead, bool /*first_read*/,
 }
 
 // Detect encoding and data rate of the track under the given drive head.
+// Do not change encoding and data rate if nothing is detected, it is probably empty track.
 bool FdrawSysDevDisk::DetectEncodingAndDataRate(int head)
 {
     FD_CMD_RESULT result{};
@@ -255,43 +256,60 @@ Track FdrawSysDevDisk::BlindReadHeaders(const CylHead& cylhead, int& firstSector
     MEMORY mem(scan_size);
     auto scan_result = reinterpret_cast<FD_TIMED_SCAN_RESULT*>(mem.pb);
 
+    bool areEncodingAndDataRateNewlyDetermined = false;
     if (m_lastEncoding == Encoding::Unknown || m_lastDataRate == DataRate::Unknown)
-        DetectEncodingAndDataRate(cylhead.head);
-
-    if (!m_fdrawcmd->CmdTimedScan(cylhead.head, scan_result, scan_size))
-        throw win32_error(GetLastError_MP(), "scan");
-
-    // If nothing was found and we have valid settings, they might have changed.
-    if (!scan_result->count && m_lastEncoding != Encoding::Unknown)
     {
-        DetectEncodingAndDataRate(cylhead.head);
+        if (!DetectEncodingAndDataRate(cylhead.head)) // No encoding and data rate. It is probably empty track.
+            return track;
+        areEncodingAndDataRateNewlyDetermined = true;
+    }
 
+    int tracktime = 0;
+    int scanAttempt;
+    for (scanAttempt = 3; scanAttempt > 0; scanAttempt--) // The scanAttempt start value could be opt parameter but not so important.
+    {
         if (!m_fdrawcmd->CmdTimedScan(cylhead.head, scan_result, scan_size))
             throw win32_error(GetLastError_MP(), "scan");
-    }
 
-    const auto tracktime = lossless_static_cast<int>(scan_result->tracktime);
-    if (opt_rpm > 0)
-    {
-        const auto rpm_time_tolerance = opt_rpm_time_tolerance_permille / 1000.;
-        // Since disk speed is not perfectly constant the tracktime might fluctuate within a tolerated range.
-        const auto rpm_time_tolerated_min = lossless_static_cast<int>(RPM_TIME_MICROSEC(opt_rpm) * (1 - rpm_time_tolerance));
-        const auto rpm_time_tolerated_max = lossless_static_cast<int>(RPM_TIME_MICROSEC(opt_rpm) * (1 + rpm_time_tolerance));
-        // If the track read time is out of allowed rpm time range, something is wrong and offsets are not perfect.
-        if (tracktime < rpm_time_tolerated_min || tracktime > rpm_time_tolerated_max)
-        {
-            Message(msgWarning, "track read time of %s is out of allowed range (%ul %c %i)",
-                CH(cylhead.cyl, cylhead.head), scan_result->tracktime,
-                    tracktime < rpm_time_tolerated_min ? '<' : '>', rpm_time_tolerated_min);
-            throw util::diskspeedwrong_exception("disk speed is wrong (track read time is out of allowed range)");
+        // If we have valid older settings ...
+        if (!areEncodingAndDataRateNewlyDetermined)
+        {   // ... and nothing was found, they might have changed.
+            if (scan_result->count == 0)
+            {
+                if (!DetectEncodingAndDataRate(cylhead.head)) // No encoding and data rate. It is very probably empty track.
+                    return track;
+
+                if (!m_fdrawcmd->CmdTimedScan(cylhead.head, scan_result, scan_size))
+                    throw win32_error(GetLastError_MP(), "scan");
+            }
+            areEncodingAndDataRateNewlyDetermined = true; // True if anything is read by detector or scanner.
         }
+
+        tracktime = lossless_static_cast<int>(scan_result->tracktime);
+        if (m_fdrawcmd->GetVersion().value < DriverVersion1_0_1_12)
+        {
+            int tracktimeCorrect;
+            if (!m_fdrawcmd->FdGetTrackTime(tracktimeCorrect)) // This also fixes tracktime rarely corrupted by CmdTimedScan.
+                throw win32_error(GetLastError_MP(), "GetTrackTime");
+            constexpr auto tracktimeTolerance = 0.01;
+            if (tracktime < tracktimeCorrect * (1 - tracktimeTolerance) || tracktime > tracktimeCorrect * (1 + tracktimeTolerance))
+            {
+                Message(msgWarning, "track time of %s is corrupted by older driver, upgrade it if possible. Retrying", CH(cylhead.cyl, cylhead.head));
+                continue; // Try again.
+            }
+        }
+        // https://en.wikipedia.org/wiki/List_of_floppy_disk_formats
+        // TODO What about Amiga HD disk with 22 sectors per track with 150 RPM? Otherwise the condition is correct.
+        if (tracktime > RPM_TIME_200)
+            throw util::diskspeedwrong_exception("index-halving cables are no longer supported (rpm <= 200)");
+        break;
     }
-    if (tracktime > RPM_TIME_200)
-        throw util::diskspeedwrong_exception("index-halving cables are no longer supported");
+    if (scanAttempt == 0)
+        throw util::diskspeedwrong_exception("disk speed is wrong (track read time is out of tolerated range)");
 
     firstSectorSeen = scan_result->firstseen;
 
-    if (scan_result->count > 0 && m_lastDataRate != DataRate::Unknown)
+    if (scan_result->count > 0)
     {
         const auto mfmbit_us = GetFmOrMfmDataBitTime(m_lastDataRate, m_lastEncoding);
         track.tracktime = tracktime;
