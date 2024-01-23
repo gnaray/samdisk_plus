c5f0bb2a49d0aaa6525a54d44bf287502253784e
Gábor Náray <drszamo@gmail.com>
2024-01-23 15:18:36 +0100

    Added new methods in FdrawSysDevDisk class to support newer reading solution.

--- src/types/fdrawsys_dev.cpp
+++ src/types/fdrawsys_dev.cpp
@@ -16,19 +16,35 @@
 #include "Disk.h"
 #include "Util.h"
 #include "win32_error.h"
+#include "RawTrackMFM.h"
+#include "OrphanDataCapableTrack.h"
+#include "TimedRawDualTrack.h"
+#include "MultiScanResult.h"
 
 #include <cstring>
 #include <memory>
 
 static auto& opt_base = getOpt<int>("base");
+static auto& opt_byte_tolerance_of_time = getOpt<int>("byte_tolerance_of_time");
 static auto& opt_datarate = getOpt<DataRate>("datarate");
+static auto& opt_debug = getOpt<int>("debug");
 static auto& opt_encoding = getOpt<Encoding>("encoding");
 static auto& opt_gaps = getOpt<int>("gaps");
 static auto& opt_newdrive = getOpt<int>("newdrive");
 static auto& opt_normal_disk = getOpt<bool>("normal_disk");
+static auto& opt_rescans = getOpt<RetryPolicy>("rescans");
 static auto& opt_retries = getOpt<RetryPolicy>("retries");
 static auto& opt_sectors = getOpt<long>("sectors");
 static auto& opt_steprate = getOpt<int>("steprate");
+
+class TrackInfo
+{
+public:
+    Encoding encoding{ Encoding::Unknown };
+    DataRate dataRate{ DataRate::Unknown };
+    int trackTime = 0;
+    int trackLenIdeal = 0;
+};
 
 class FdrawSysDevDisk final : public DemandDisk
 {
@@ -86,9 +102,19 @@ private:
     void ReadSector(const CylHead& cylhead, Track& track, int index, int firstSectorSeen = 0);
     void ReadFirstGap(const CylHead& cylhead, Track& track);
 
+    /* Here are the methods of multi track reading based on CmdTimedMultiScan
+     * implemented in driver version >= 1.0.1.12.
+     */
+    bool ScanAndDetectIfNecessary(const CylHead& cylhead, MultiScanResult& multiScanResult);
+    TimedRawDualTrack BlindReadHeaders112(const CylHead& cylhead, const DeviceReadingPolicy& deviceReadingPolicy);
+    void GuessAndAddSectorIdsOfOrphans(Track& track, TimedRawDualTrack& timedRawDualTrack) const;
+    static bool GetSectorDataFromRawTrack(TimedRawDualTrack& timedRawDualTrack, const int index);
+    bool ReadSectors(const CylHead& cylhead, TimedRawDualTrack& timedRawDualTrack, const DeviceReadingPolicy& deviceReadingPolicy);
+    bool ReadAndMergeRawTracks(const CylHead& cylhead, TimedRawDualTrack& timedRawDualTrack);
     std::unique_ptr<FdrawcmdSys> m_fdrawcmd;
     Encoding m_lastEncoding{ Encoding::Unknown };
     DataRate m_lastDataRate{ DataRate::Unknown };
+    TrackInfo m_trackInfo[MAX_DISK_CYLS * MAX_DISK_HEADS];
     bool m_warnedMFM128{ false };
 };
 
@@ -485,6 +511,314 @@ void FdrawSysDevDisk::ReadFirstGap(const CylHead& cylhead, Track& track)
         break;
     }
 }
+
+///////////////////////////////////////////////////////////////////////////////
+/* Here are the methods of multi track reading based on CmdTimedMultiScan
+ * implemented in driver version >= 1.0.1.12.
+ */
+bool FdrawSysDevDisk::ScanAndDetectIfNecessary(const CylHead& cylhead, MultiScanResult &multiScanResult)
+{
+    const bool trackInfoHasEncodingAndDataRate = m_trackInfo[cylhead].encoding != Encoding::Unknown && m_trackInfo[cylhead].dataRate != DataRate::Unknown;
+    if (trackInfoHasEncodingAndDataRate && (m_trackInfo[cylhead].encoding != m_lastEncoding || m_trackInfo[cylhead].dataRate != m_lastDataRate))
+    {
+        m_lastEncoding = m_trackInfo[cylhead].encoding;
+        m_lastDataRate = m_trackInfo[cylhead].dataRate;
+        if (!m_fdrawcmd->SetEncRate(m_lastEncoding, m_lastDataRate))
+            throw win32_error(GetLastError_MP(), "SetEncRate");
+    }
+    if (m_lastEncoding != Encoding::Unknown && m_lastDataRate != DataRate::Unknown)
+    {
+        // Try the last successful encoding and data rate.
+        if (!m_fdrawcmd->CmdTimedMultiScan(cylhead.head, 0, multiScanResult, multiScanResult.size, opt_byte_tolerance_of_time))
+            throw win32_error(GetLastError_MP(), "TimedMultiScan");
+        if (trackInfoHasEncodingAndDataRate) // Not found sector but earlier found so detection is successful.
+            return true;
+        // Return true if we found a sector.
+        if (multiScanResult.count() > 0) // Found sector so detection is successful.
+        {
+            m_trackInfo[cylhead].encoding = m_lastEncoding;
+            m_trackInfo[cylhead].dataRate = m_lastDataRate;
+            return true;
+        }
+    }
+
+    // Prefer MFM to FM.
+    for (auto encoding : { Encoding::MFM, Encoding::FM })
+    {
+        // Prefer higher datarates.
+        for (auto dataRate : { DataRate::_1M, DataRate::_500K, DataRate::_300K, DataRate::_250K })
+        {
+            // Skip FM if we're only looking for MFM, or the data rate is 1Mbps.
+            if (encoding == Encoding::FM && (opt_encoding == Encoding::MFM || dataRate == DataRate::_1M))
+                continue;
+
+            // Skip rates not matching user selection.
+            if (opt_datarate != DataRate::Unknown && dataRate != opt_datarate)
+                continue;
+
+            // Skip 1Mbps if the FDC doesn't report it's supported.
+            if (dataRate == DataRate::_1M)
+            {
+                FD_FDC_INFO fi{};
+                if (!m_fdrawcmd->GetFdcInfo(fi) || !(fi.SpeedsAvailable & FDC_SPEED_1M))
+                {
+                    // Fail if user selected the rate.
+                    if (opt_datarate == DataRate::_1M)
+                        throw util::exception("FDC doesn't support 1Mbps data rate");
+
+                    continue;
+                }
+            }
+
+            if (!m_fdrawcmd->SetEncRate(encoding, dataRate))
+                throw win32_error(GetLastError_MP(), "SetEncRate");
+
+            if (!m_fdrawcmd->CmdTimedMultiScan(cylhead.head, 0, multiScanResult, multiScanResult.size, opt_byte_tolerance_of_time))
+                throw win32_error(GetLastError_MP(), "TimedMultiScan");
+            if (multiScanResult.count() > 0)
+            {
+                // Remember the settings for the first try next time.
+                m_lastEncoding = encoding;
+                m_lastDataRate = dataRate;
+                m_trackInfo[cylhead].encoding = encoding;
+                m_trackInfo[cylhead].dataRate = dataRate;
+                return true;
+            }
+        }
+    }
+
+    return false; // Nothing detected.
+}
+
+TimedRawDualTrack FdrawSysDevDisk::BlindReadHeaders112(const CylHead& cylhead, const DeviceReadingPolicy& deviceReadingPolicy)
+{
+    if (m_fdrawcmd->GetVersion().value < DriverVersion1_0_1_12)
+        throw util::exception("BlindReadHeaders112 method requires driver version 1.12 at least");
+
+    if (m_trackInfo[cylhead].trackTime <= 0)
+    {   // Premeasuring tracktime once would be enough for constant speed drive but would not be for variable speed drives.
+        // In addition CmdTimed*Scan measures tracktime only if it was not measured earlier although CmdTimed*Scan might adjust it.
+        FD_MULTI_TRACK_TIME_RESULT track_time;
+        if (!m_fdrawcmd->FdGetMultiTrackTime(track_time, 1))
+            throw win32_error(GetLastError_MP(), "GetMultiTrackTime"); // "not available for this disk type"
+        m_trackInfo[cylhead].trackTime = lossless_static_cast<int>(track_time.spintime);
+        // variable speed is out of scope. however this solution supports variable speed unless m_trackInfo[all] is set as spintime.
+    }
+
+    TimedRawDualTrack timedRawDualTrack;
+    // Find the sectors by scanning the floppy disk both timed and raw.
+    auto rawTrackRescans = std::max(opt_rescans, opt_retries);
+    DeviceReadingPolicy deviceReadingPolicyForScanning = deviceReadingPolicy;
+    auto timedTrackRescans = opt_rescans;
+    do
+    {
+        MultiScanResult multiScanResult(MAX_SECTORS);
+        if (!ScanAndDetectIfNecessary(cylhead, multiScanResult))
+            return timedRawDualTrack;
+
+        auto timedTrackTime = multiScanResult.trackTime();
+        // https://en.wikipedia.org/wiki/List_of_floppy_disk_formats
+        // TODO What about Amiga HD disk with 150 RPM? Otherwise the condition is correct.
+        if (timedTrackTime > RPM_TIME_200)
+            throw util::diskspeedwrong_exception("index-halving cables are no longer supported (rpm <= 200)");
+        m_trackInfo[cylhead].trackTime = timedTrackTime;
+        if (m_lastEncoding != Encoding::MFM) // Currently only MFM encoding is supported due to ReadAndMergeRawTracks method.
+            return timedRawDualTrack;
+
+        if (multiScanResult.count() > 0)
+        {
+            if (m_trackInfo[cylhead].trackLenIdeal <= 0)
+            {
+                ReadAndMergeRawTracks(cylhead, timedRawDualTrack);
+                rawTrackRescans--;
+            }
+            Track newTimedTrack = multiScanResult.DecodeResult(cylhead, m_lastDataRate, m_lastEncoding, m_trackInfo[cylhead].trackLenIdeal);
+            if (m_trackInfo[cylhead].trackLenIdeal > 0)
+                newTimedTrack.setTrackLenAndNormaliseTrackTimeAndSectorOffsets(m_trackInfo[cylhead].trackLenIdeal);
+
+            const auto sectorAmountPrev = timedRawDualTrack.timedTrack.size();
+            timedRawDualTrack.timedTrack.add(std::move(newTimedTrack));
+            if (timedRawDualTrack.timedTrack.size() > sectorAmountPrev)
+            {
+                deviceReadingPolicyForScanning = deviceReadingPolicy;
+                deviceReadingPolicyForScanning.AddSkippableSectors(timedRawDualTrack.timedTrack.good_idcrc_sectors());
+                if (timedTrackRescans.sinceLastChange)
+                    timedTrackRescans = opt_rescans;
+            }
+        }
+    } while (timedTrackRescans-- > 0 && deviceReadingPolicyForScanning.WantMoreSectors());
+
+    // If more scanning is required then try to find sectors from the raw track.
+    do
+    {
+        bool foundNewSector = rawTrackRescans >= 0 && ReadAndMergeRawTracks(cylhead, timedRawDualTrack);
+        if (m_trackInfo[cylhead].trackLenIdeal > 0 && (foundNewSector || timedRawDualTrack.lastTimedRawTrackSingle.empty()))
+        {
+            if (timedRawDualTrack.SyncAndDemultiRawToTimed(m_trackInfo[cylhead].trackLenIdeal)) // Updates lastTimedRawTrackSingle.
+            {
+                const auto sectorAmountPrev = timedRawDualTrack.finalTimedAndRawTrack.size();
+                // Merging last timed raw track with timed track and orphan ids into final timed and raw track.
+                auto finalTimedAndRawTrackLocal = timedRawDualTrack.lastTimedRawTrackSingle.track;
+                finalTimedAndRawTrackLocal.add(Track(timedRawDualTrack.timedTrack));
+                GuessAndAddSectorIdsOfOrphans(finalTimedAndRawTrackLocal, timedRawDualTrack);
+                finalTimedAndRawTrackLocal.add(std::move(timedRawDualTrack.finalTimedAndRawTrack));
+                timedRawDualTrack.finalTimedAndRawTrack = finalTimedAndRawTrackLocal;
+                if (timedRawDualTrack.finalTimedAndRawTrack.size() > sectorAmountPrev)
+                {
+                    deviceReadingPolicyForScanning = deviceReadingPolicy;
+                    deviceReadingPolicyForScanning.AddSkippableSectors(timedRawDualTrack.finalTimedAndRawTrack.good_idcrc_sectors());
+                }
+            }
+        }
+        if (!deviceReadingPolicyForScanning.WantMoreSectors())
+        {
+            if (ReadSectors(cylhead, timedRawDualTrack, deviceReadingPolicy))
+                break; // Scanning and reading is complete.
+        }
+        if (foundNewSector)
+        {
+            if (rawTrackRescans.sinceLastChange)
+                rawTrackRescans = std::max(opt_rescans, opt_retries);
+        }
+    } while (rawTrackRescans-- > 0);
+
+    // Remove not normal sector headers at the track end.
+    if (opt_normal_disk)
+    {
+        while (!timedRawDualTrack.finalTimedAndRawTrack.empty())
+        {
+            auto it = timedRawDualTrack.finalTimedAndRawTrack.rbegin();
+            const auto& sector = *it;
+            const auto lengthWithoutOuterGaps = GetFmOrMfmSectorOverheadFromOffsetToDataCrcEnd(sector.datarate, sector.encoding, sector.size());
+            if (sector.offset + lengthWithoutOuterGaps * 8 * 2 < timedRawDualTrack.finalTimedAndRawTrack.tracklen) // It fits, no problem.
+                break;
+            timedRawDualTrack.finalTimedAndRawTrack.sectors().erase(std::next(it).base());
+        }
+    }
+
+    ReadSectors(cylhead, timedRawDualTrack, deviceReadingPolicy);
+
+    return timedRawDualTrack;
+}
+
+void FdrawSysDevDisk::GuessAndAddSectorIdsOfOrphans(Track& track, TimedRawDualTrack& timedRawDualTrack) const
+{
+    // If there is no cylhead mismatch and there are orphan datas then guess their sector id.
+    if (timedRawDualTrack.lastTimedRawTrackSingle.cylheadMismatch || timedRawDualTrack.lastTimedRawTrackSingle.orphanDataTrack.empty())
+        return;
+    IdAndOffsetVector idAndOffsetVector;
+    for (const auto& orphanDataSector : timedRawDualTrack.lastTimedRawTrackSingle.orphanDataTrack)
+    {
+
+        if (opt_debug)
+            util::cout << "GuessAndAddSectorIdsOfOrphans: processing sector, offset=" << orphanDataSector.offset << ", ID=" << orphanDataSector.header.sector << "\n";
+
+        const auto it = track.findForDataFmOrMfm(orphanDataSector.offset, orphanDataSector.data_size());
+        if (it == track.end()) // The end is dynamic since adding sector to track.
+        {
+            util::cout << "GuessAndAddSectorIdsOfOrphans: Orphan has no parent, offset=" << orphanDataSector.offset << "\n";
+            if (idAndOffsetVector.empty())
+            {
+                // Let us discover the format scheme if possible.
+                idAndOffsetVector = track.DiscoverTrackSectorScheme();
+                if (idAndOffsetVector.empty())
+                    return;
+            }
+            // Missing sector ID, try to find it in the found format scheme.
+            const auto sectorId = orphanDataSector.FindParentSectorIdByOffset(idAndOffsetVector);
+            if (sectorId >= 0)
+            {
+                auto parentSector = orphanDataSector;
+                parentSector.header.sector = sectorId;
+                parentSector.offset = track.findReasonableIdOffsetForDataFmOrMfm(orphanDataSector.offset);
+                track.add(std::move(parentSector));
+            }
+        }
+    }
+}
+
+/*static*/ bool FdrawSysDevDisk::GetSectorDataFromRawTrack(TimedRawDualTrack& timedRawDualTrack, const int index)
+{
+    auto& finalTrack = timedRawDualTrack.finalTimedAndRawTrack;
+    auto& sector = finalTrack[index];
+
+    if (sector.has_badidcrc())
+        return false;
+    if (sector.has_good_data(false, opt_normal_disk))
+        return true;
+
+    if (opt_debug)
+        util::cout << "RawReadSector: reading " << index << ". sector having ID " << sector.header.sector << "\n";
+
+    // If there is no cylhead mismatch and there are orphan datas then try to find the orphan data of sector.
+    if (timedRawDualTrack.lastTimedRawTrackSingle.cylheadMismatch || timedRawDualTrack.lastTimedRawTrackSingle.orphanDataTrack.empty())
+        return false;
+    const auto it = timedRawDualTrack.lastTimedRawTrackSingle.orphanDataTrack.findDataForSectorIdFmOrMfm(sector.offset, sector.size());
+    if (it != timedRawDualTrack.lastTimedRawTrackSingle.orphanDataTrack.end())
+    {
+        if (it->has_good_data(false, opt_normal_disk))
+        {
+            sector.add_with_readstats(Data(it->data_copy()));
+            return true;
+        }
+    }
+    return false;
+}
+
+bool FdrawSysDevDisk::ReadSectors(const CylHead& cylhead, TimedRawDualTrack& timedRawDualTrack, const DeviceReadingPolicy& deviceReadingPolicy)
+{
+    // Limiting sector reading as specified in case of normal disk request.
+    const auto normal_sector_id_begin = opt_base > 0 ? opt_base : 1;
+    const auto normal_sector_id_end = opt_sectors > 0 ? (normal_sector_id_begin + opt_sectors) : 256;
+
+    bool allDataGood = true;
+    const auto iSup = timedRawDualTrack.finalTimedAndRawTrack.size();
+    for (int j = 0; j < 2; j++)
+    {
+        for (int i = j; i < iSup; i += 2)
+        {
+            const auto& sector = timedRawDualTrack.finalTimedAndRawTrack[i];
+            if ((!deviceReadingPolicy.SkippableSectors().Contains(sector, timedRawDualTrack.finalTimedAndRawTrack.tracklen)
+                 && (!opt_normal_disk || (sector.header.sector >= normal_sector_id_begin && sector.header.sector < normal_sector_id_end))))
+            {
+                if (sector.read_attempts() == 0)
+                    ReadSector(cylhead, timedRawDualTrack.finalTimedAndRawTrack, i, 0);
+                if (!GetSectorDataFromRawTrack(timedRawDualTrack, i))
+                    allDataGood = false; // Sector is still not good.
+            }
+        }
+    }
+    return allDataGood;
+}
+
+bool FdrawSysDevDisk::ReadAndMergeRawTracks(const CylHead& cylhead, TimedRawDualTrack& timedRawDualTrack)
+{
+    assert(m_lastEncoding == Encoding::MFM); // Currently this method handles only MFM track.
+    assert(m_lastDataRate != DataRate::Unknown);
+    MEMORY mem;
+
+    if (!m_fdrawcmd->CmdReadTrack(cylhead.head, cylhead.cyl, cylhead.head, 0, 8, 1, mem)) // Read one big 32K sector.
+        throw win32_error(GetLastError_MP(), "ReadTrack");
+    RawTrackMFM toBeMergedRawTrack(mem, m_lastDataRate);
+    const auto sectorIdAmountPrev = timedRawDualTrack.rawTrackMulti.track.size();
+    const auto sectorAmountPrev = timedRawDualTrack.rawTrackMulti.size();
+    timedRawDualTrack.rawTrackMulti.mergeRawTrack(cylhead, toBeMergedRawTrack);
+    const bool foundNewSectorId = timedRawDualTrack.rawTrackMulti.track.size() > sectorIdAmountPrev;
+    const bool foundNewSector = timedRawDualTrack.rawTrackMulti.size() > sectorAmountPrev;
+    if (m_trackInfo[cylhead].trackLenIdeal <= 0 && foundNewSectorId)
+    {
+        const auto bestTrackLen = timedRawDualTrack.rawTrackMulti.determineBestTrackLen(GetFmOrMfmTimeDataBitsAsRounded(m_lastDataRate, m_lastEncoding, m_trackInfo[cylhead].trackTime));
+        if (bestTrackLen > 0)
+        {
+            m_trackInfo[cylhead].trackLenIdeal = bestTrackLen;
+            if (timedRawDualTrack.timedTrack.tracklen > 0)
+                timedRawDualTrack.timedTrack.setTrackLenAndNormaliseTrackTimeAndSectorOffsets(m_trackInfo[cylhead].trackLenIdeal);
+        }
+    }
+    return foundNewSector;
+}
+
+///////////////////////////////////////////////////////////////////////////////
 
 bool ReadFdrawcmdSys(const std::string& path, std::shared_ptr<Disk>& disk)
 {
