479781214abbc9cafe507e6b787963c8ef7d8bc0
Gábor Náray <drszamo@gmail.com>
2024-02-01 10:58:17 +0100

    Cosmetical change, removed commented includes in SAMdisk.h.

--- include/SAMdisk.h
+++ include/SAMdisk.h
@@ -1,17 +1,10 @@
 #pragma once
 
 #include "PlatformConfig.h"
-//#include "utils.h"
-//#include "win32_error.h"
-//#include "CRC16.h"
 #include "Disk.h"
 #include "DiskUtil.h"
 #include "Header.h"
-//#include "MemFile.h"
-//#include "Image.h"
 #include "HDD.h"
-//#include "Util.h"
-//#include "SAMCoupe.h"
 
 // copy
 bool ImageToImage(const std::string& src_path, const std::string& dst_path);
